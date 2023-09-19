cfdb40fd99deef73360685453bd9162bc3391b89
Shiningdark0930 <https://github.com/Shiningdark0930>
2023-09-19 15:28:45 +0900

    week3

--- Week3_3.py
+++ Week3_3.py
@@ -15,7 +15,19 @@ class Book(Item):
         self.auther = auther
 
     def __str__(self):
-        print("제목:", self.title,",가격:", self.price,",페이지 수:", self.page,",저자:", self.auther)
+        return f"제목:{self.title}, 가격:{self.price}, 페이지:{self.page}, 저자:{self.auther}"
+
+    def getprice(self):
+        print(self.title,"의 가격은", self.price,"원 입니다.")
+
+class Magazine(Item):
+    def __init__(self, title, price, date):
+        self.title = title
+        self.price = price
+        self.date = date
+
+    def __str__(self):
+        return f"제목:{self.title}, 가격:{self.price}, 출간 월:{self.date}"
 
     def getprice(self):
         print(self.title,"의 가격은", self.price,"원 입니다.")
@@ -24,5 +36,10 @@ if __name__ == '__main__':
     book1 = Book('소나기', 10000, 123, '황순원')
     book2 = Book('메밀꽃 필 무렵', 15000, 142, '이효석')
     book3 = Book('난쟁이가 쏘아올린 작은 공', 12000, 112, '조세희')
-    print('', book1,'\n', book2, '\n', book3, '\n')
-
+    magazine1 = Magazine('보그',11000, 9)
+    magazine2 = Magazine('싱글즈',13000, 8)
+    print('', book1,'\n', book2, '\n', book3, '\n', magazine1, '\n', magazine2)
+    print('총',Item.count,'권')
+    book2.getprice()
+    magazine1.getprice()
+    book1.getprice()
