baaf9e389096bcb80a0b935b0635923be6faaefb
Shiningdark0930 <https://github.com/Shiningdark0930>
2023-09-19 14:45:57 +0900

    week3

--- Week3_3.py
+++ Week3_3.py
@@ -15,7 +15,7 @@ class Book(Item):
         self.auther = auther
 
     def __str__(self):
-        print("제목:", self.title,"가격:", self.price,"페이지 수:", self.page,"저자:", self.auther)
+        print("제목:", self.title,",가격:", self.price,",페이지 수:", self.page,",저자:", self.auther)
 
     def getprice(self):
         print(self.title,"의 가격은", self.price,"원 입니다.")
@@ -24,4 +24,5 @@ if __name__ == '__main__':
     book1 = Book('소나기', 10000, 123, '황순원')
     book2 = Book('메밀꽃 필 무렵', 15000, 142, '이효석')
     book3 = Book('난쟁이가 쏘아올린 작은 공', 12000, 112, '조세희')
-    print(book1,'\n',book2,'\n',book3,'\n')
+    print('', book1,'\n', book2, '\n', book3, '\n')
+
